6f4e7cd35ee1f3f2d275350a04621337f9a68f16
Mishel450 <tomasmishel18@gmai.com>
2023-08-21 09:25:18 -0700

    0-gather_data_from_an_API.py 2.0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,16 +9,18 @@ if __name__ == "__main__":
     task_done = 0
     task_count = 0
     id = int(argv[1])
-    r_users = requests.get(req.format(id)).json()
-    r_todos = requests.get(req_all_task).json()
-    for i in r_todos:
+    r_users = requests.get(req.format(id))
+    j_users = r_users.json()
+    r_todos = requests.get(req_all_task)
+    j_todos = r_todos.json()
+    for i in j_todos:
         if i['userId'] == id:
             task_count += 1
         if i['userId'] == id and i['completed'] is True:
             task_done += 1
-    EMPLOYEE_NAME = r_users['name']
+    EMPLOYEE_NAME = j_users['name']
     print(f"Employee {EMPLOYEE_NAME} is done with tasks({task_done}/{task_count}):")
-    for i in r_todos:
+    for i in j_todos:
         if i['userId'] == id and i['completed'] is True:
             print(f"\t {i['title']}")
 
