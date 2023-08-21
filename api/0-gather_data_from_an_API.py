a8bb614709fbac381fa0f140c191b4688f2c6e9c
Mishel450 <tomasmishel18@gmai.com>
2023-08-21 05:37:16 -0700

    0-gather_data_from_an_API.py done

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    txt1 = "Employee {} is done with tasks({}/{}):"
+    txt2 = "    {}"
+    r = requests.get(argv[1])
+    print(txt1.format(r['EMPLOYEE_NAME'], r['NUMBER_OF_DONE_TASKS'],
+                      r['TOTAL_NUMBER_OF_TASKS']))
+    print(txt2.format(r['TASK_TITLE']))
