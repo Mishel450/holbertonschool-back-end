4b8f441d9af10035f1039a13d4d52cb8d676f44a
Mishel450 <tomasmishel18@gmai.com>
2023-08-21 06:42:25 -0700

    0-gather_data_from_an_API.py 1.2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,8 +5,21 @@ from sys import argv
 
 if __name__ == "__main__":
     txt1 = "Employee {} is done with tasks({}/{}):"
-    txt2 = "    {}"
-    r = requests.get(argv[1])
-    print(txt1.format(r['EMPLOYEE_NAME'], r['NUMBER_OF_DONE_TASKS'],
-                      r['TOTAL_NUMBER_OF_TASKS']))
-    print(txt2.format(r['TASK_TITLE']))
+    txt2 = "     {}"
+    req = "https://jsonplaceholder.typicode.com/users/{}"
+    req_all_task = "https://jsonplaceholder.typicode.com/todos"
+    task_done = 0
+    task_count = 0
+    id = int(argv[1])
+    r_users = requests.get(req.format(id)).json()
+    r_todos = requests.get(req_all_task).json()
+    for i in r_todos:
+        if i['userId'] == id:
+            task_count += 1
+        if i['userId'] == id and i['completed'] is True:
+            task_done += 1
+    print(txt1.format(r_users['name'], task_done,
+                      task_count))
+    for i in r_todos:
+        if i['userId'] == id and i['completed'] is True:
+            print(txt2.format(i['title']))
